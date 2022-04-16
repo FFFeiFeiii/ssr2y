487b7becb4160ea8461f2e200de00292aa203674
FancyBurn <66993322+FancyBurn@users.noreply.github.com>
2022-04-17 01:30:23 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -7,6 +7,8 @@ curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
 install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
 install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+install -m 755 /tmp/v2ray/geoip.dat /usr/local/share/v2ray/geoip.dat
+install -m 755 /tmp/v2ray/geosite.dat /usr/local/share/v2ray/geosite.dat
 # Remove temporary directory
 rm -rf /tmp/v2ray
 # V2Ray new configuration
