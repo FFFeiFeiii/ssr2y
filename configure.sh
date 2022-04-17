fa8ac975b43b94e4cc6868bcf17b73db885a3156
FancyBurn <66993322+FancyBurn@users.noreply.github.com>
2022-04-17 09:51:31 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,18 +2,15 @@
 # V2Ray generate configuration
 # Download and install V2Ray
 config_path=$PROTOCOL"_ws_tls.json"
-#mkdir /tmp/v2ray
-#curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-#unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
-#install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
-#install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
-#install -m 644 /tmp/v2ray/geoip.dat /usr/local/share/v2ray/geoip.dat
-#install -m 644 /tmp/v2ray/geosite.dat /usr/local/share/v2ray/geosite.dat
-## Remove temporary directory
-#rm -rf /tmp/v2ray
-
-
-
+mkdir /tmp/v2ray
+curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
+install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
+install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+install -m 644 /tmp/v2ray/geoip.dat /usr/local/share/v2ray/geoip.dat
+install -m 644 /tmp/v2ray/geosite.dat /usr/local/share/v2ray/geosite.dat
+# Remove temporary directory
+rm -rf /tmp/v2ray
 
 # V2Ray new configuration
 install -d /usr/local/etc/v2ray
